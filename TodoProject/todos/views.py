dc3f2f5f356c101460fb65fb21dff92a48f35db1
nburkett <nicburkett@gmail.com>
2022-04-13 16:47:58 -0500

    econd commit, working app

--- TodoProject/todos/views.py
+++ TodoProject/todos/views.py
@@ -4,18 +4,15 @@ from .models import Todo
 
 # Create your views here.
 
-
 def list_todo_items(request):
     context = {'todo_list' : Todo.objects.all()}
     return render(request, 'todos/todo_list.html',context)
 
 
 def insert_todo_item(request: HttpRequest):
-    content = request.POST['content'] #take the 
-
-    # todo = Todo(content=request.POST['content'])
-    # todo.save()
-    # return redirect('/todos/list/')
+    todo = Todo(content=request.POST['content'])  #take the content from the html form and define as a variable 
+    todo.save()
+    return redirect('/todos/list/')
 
 def delete_todo_item(request,todo_id):
     todo_to_delete = Todo.objects.get(id=todo_id)
